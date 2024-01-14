1a1e00ee26b52547594bb416255812bf476a1527
krishna <officialkrishna009@gmail.com>
2024-01-14 18:53:01 +0545

    Create javascript.js

---
+++ javascript.js
@@ -0,0 +1,8 @@
+function validate_newsletter(){
+    var email = document.forms['news-form']['newsletter'].value;
+    if (email != "") {
+        alert("Thank u for Signing up for Newsletter");
+    }
+    
+    
+}
