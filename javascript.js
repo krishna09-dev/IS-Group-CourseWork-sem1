c66c4297766a21dd6ee913a22228453f2343a8d1
krishna <officialkrishna009@gmail.com>
2024-01-31 14:54:26 +0545

    updated

--- javascript.js
+++ javascript.js
@@ -1,3 +1,4 @@
+//newletter form validation
 function validate_newsletter(){
     var email = document.forms['news-form']['newsletter'].value;
     if (email != "") {
@@ -6,3 +7,21 @@ function validate_newsletter(){
     
     
 }
+
+//checkout form validation
+function validate() {
+    var fullname = document.forms['checkoutform']['fullname'].value;
+    var youremail = document.forms['checkoutform']['youremail'].value;
+    var yournumber = document.forms['checkoutform']['yournumber'].value;
+    var yourcountry = document.forms['checkoutform']['yourcountry'].value;
+    var yourcity = document.forms['checkoutform']['yourcity'].value;  
+    var yourzip = document.forms['checkoutform']['yourzip'].value;    
+    var yourbilling = document.forms['checkoutform']['yourbilling'].value;
+
+    if (fullname == "" || youremail == "" || yournumber == "" || yourcountry == "" || yourcity == "" || yourzip == "" || yourbilling == "") {
+        alert("all fields must be filled out");
+    }else {
+        alert("Your order has been placed");
+    }
+}
+
